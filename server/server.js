bdd6ea2bf02a4e5697daf12ec3e9f455d2dc6b48
Hector_GIraldo <hectorgiraldo0210@gmail.com>
2020-04-27 14:07:34 +0200

    Login normal

--- server/server.js
+++ server/server.js
@@ -12,7 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(require('./routes/usuario'));
+
+// Configuracion global rutas
+app.use(require('./routes/index'));
 
 app.get('/', function(req, res) {
     res.json('Hello World')
